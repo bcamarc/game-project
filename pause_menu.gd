8bc93511bba7a99aadb05b3380215c4da2088953
and-god <andgod28@bergen.org>
2026-05-25 14:08:35 -0400

    pause menu

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,13 +1,16 @@
 extends CanvasLayer
+@onready var button: BaseButton = %Resume
+
 func _ready() -> void:
 	hide()
-func _process(delta: float) -> void:
-	if (Input.is_action_just_released("pause")):
-		if (visible):
-			hide()
-		else:
-			show()
-	if (visible):
-		get_tree().paused = true
-	else:
-		get_tree().paused = false
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	button.pressed.connect(func(): toggle_menu())
+	
+func toggle_menu() -> void:
+	visible = !visible
+	get_tree().paused = visible
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		toggle_menu()
+		get_viewport().set_input_as_handled()
