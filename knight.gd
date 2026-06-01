976663d7b8e50de7525fec5bce3445a2b5dc92bc
and-god <andgod28@bergen.org>
2026-06-01 13:09:45 -0400

    hitbox and character menu

--- knight.gd
+++ knight.gd
@@ -1,9 +1,10 @@
-extends Sprite2D
-
+extends CharacterBody2D
+@onready var sprite = $KnightSprite
 var pos: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	pass 
 	
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	sprite.play("menu")
+	
